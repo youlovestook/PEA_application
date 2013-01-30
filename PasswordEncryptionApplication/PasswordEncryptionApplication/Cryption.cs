7185d25b3b8edb99cd53ddf0de04a407f0e445a8
ebearden <elvin.bearden@gmail.com>
2013-01-30 16:05:15 -0600

    Added MainForm Layout.

--- PasswordEncryptionApplication/PasswordEncryptionApplication/Cryption.cs
+++ PasswordEncryptionApplication/PasswordEncryptionApplication/Cryption.cs
@@ -7,6 +7,44 @@ namespace PasswordEncryptionApplication
 {
     class Cryption
     {
+        private String masterKey;
 
+        /// <summary>
+        /// Super weak encryption routine to test other functionality.
+        /// </summary>
+        /// <param name="toEncrypt">String to encrypt</param>
+        /// <returns>Encrypted String</returns>
+        public String Encrypt(String toEncrypt)
+        {
+            char[] encrypt = toEncrypt.ToCharArray();
+
+            for (int i = 0; i < encrypt.Length; i++)
+            {
+                encrypt[i] = (char)((int)encrypt[i] * 2);
+            }
+
+            StringBuilder strBuilder = new StringBuilder();
+            strBuilder.Append(encrypt);
+            return strBuilder.ToString();
+        }
+
+        /// <summary>
+        /// Decrypt a String
+        /// </summary>
+        /// <param name="toDecrypt">String to be decrypted.</param>
+        /// <returns>Decrypted String</returns>
+        public String Decrypt(String toDecrypt)
+        {
+            char[] decrypt = toDecrypt.ToCharArray();
+
+            for (int i = 0; i < decrypt.Length; i++)
+            {
+                decrypt[i] = (char)((int)decrypt[i] / 2);
+            }
+
+            StringBuilder strBuilder = new StringBuilder();
+            strBuilder.Append(decrypt);
+            return strBuilder.ToString();
+        }
     }
 }
--- PasswordEncryptionApplication/PasswordEncryptionApplication/MainForm.Designer.cs
+++ PasswordEncryptionApplication/PasswordEncryptionApplication/MainForm.Designer.cs
@@ -29,50 +29,155 @@
         private void InitializeComponent()
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.domainTab = new System.Windows.Forms.TabPage();
+            this.noteTab = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.masterKeyTextBox = new System.Windows.Forms.TextBox();
+            this.masterKeyEnterBtn = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.editBtn = new System.Windows.Forms.Button();
+            this.removeBtn = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(38, 61);
+            this.tabControl1.Controls.Add(this.domainTab);
+            this.tabControl1.Controls.Add(this.noteTab);
+            this.tabControl1.Location = new System.Drawing.Point(16, 65);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(200, 100);
+            this.tabControl1.Size = new System.Drawing.Size(464, 332);
             this.tabControl1.TabIndex = 0;
             // 
-            // tabPage1
+            // domainTab
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(192, 74);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.domainTab.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.domainTab.Location = new System.Drawing.Point(4, 22);
+            this.domainTab.Name = "domainTab";
+            this.domainTab.Padding = new System.Windows.Forms.Padding(3);
+            this.domainTab.Size = new System.Drawing.Size(456, 306);
+            this.domainTab.TabIndex = 0;
+            this.domainTab.Text = "Domains";
+            this.domainTab.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // noteTab
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.noteTab.Location = new System.Drawing.Point(4, 22);
+            this.noteTab.Name = "noteTab";
+            this.noteTab.Padding = new System.Windows.Forms.Padding(3);
+            this.noteTab.Size = new System.Drawing.Size(456, 306);
+            this.noteTab.TabIndex = 1;
+            this.noteTab.Text = "Notes";
+            this.noteTab.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.masterKeyEnterBtn);
+            this.groupBox1.Controls.Add(this.masterKeyTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(16, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(219, 46);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Master Key:";
+            // 
+            // masterKeyTextBox
+            // 
+            this.masterKeyTextBox.Location = new System.Drawing.Point(7, 20);
+            this.masterKeyTextBox.Name = "masterKeyTextBox";
+            this.masterKeyTextBox.Size = new System.Drawing.Size(156, 20);
+            this.masterKeyTextBox.TabIndex = 0;
+            this.masterKeyTextBox.UseSystemPasswordChar = true;
+            // 
+            // masterKeyEnterBtn
+            // 
+            this.masterKeyEnterBtn.Location = new System.Drawing.Point(169, 18);
+            this.masterKeyEnterBtn.Name = "masterKeyEnterBtn";
+            this.masterKeyEnterBtn.Size = new System.Drawing.Size(43, 23);
+            this.masterKeyEnterBtn.TabIndex = 1;
+            this.masterKeyEnterBtn.Text = "Enter";
+            this.masterKeyEnterBtn.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.searchBtn);
+            this.groupBox2.Controls.Add(this.searchTextBox);
+            this.groupBox2.Location = new System.Drawing.Point(252, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(228, 46);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Domain Search:";
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(7, 20);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(167, 20);
+            this.searchTextBox.TabIndex = 0;
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(181, 18);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(36, 23);
+            this.searchBtn.TabIndex = 1;
+            this.searchBtn.Text = "Go";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(16, 403);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(75, 23);
+            this.addBtn.TabIndex = 3;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            // 
+            // editBtn
+            // 
+            this.editBtn.Enabled = false;
+            this.editBtn.Location = new System.Drawing.Point(97, 403);
+            this.editBtn.Name = "editBtn";
+            this.editBtn.Size = new System.Drawing.Size(75, 23);
+            this.editBtn.TabIndex = 3;
+            this.editBtn.Text = "Edit";
+            this.editBtn.UseVisualStyleBackColor = true;
+            // 
+            // removeBtn
+            // 
+            this.removeBtn.Enabled = false;
+            this.removeBtn.Location = new System.Drawing.Point(178, 403);
+            this.removeBtn.Name = "removeBtn";
+            this.removeBtn.Size = new System.Drawing.Size(75, 23);
+            this.removeBtn.TabIndex = 3;
+            this.removeBtn.Text = "Remove";
+            this.removeBtn.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(496, 438);
+            this.Controls.Add(this.removeBtn);
+            this.Controls.Add(this.editBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.tabControl1);
             this.Name = "MainForm";
             this.Text = "Form1";
             this.tabControl1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -80,8 +185,17 @@
         #endregion
 
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage noteTab;
+        private System.Windows.Forms.TabPage domainTab;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button masterKeyEnterBtn;
+        private System.Windows.Forms.TextBox masterKeyTextBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button editBtn;
+        private System.Windows.Forms.Button removeBtn;
     }
 }
 
